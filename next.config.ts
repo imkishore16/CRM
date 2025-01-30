ceb79daceb93a742916eb11836fbdeb05c343156
kishore <imkishor16@gmail.com>
2025-01-31 05:01:44 +0530

    :)

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,34 @@
 // export default nextConfig;
 
 
-/** @type {import('next').NextConfig} */
+import type { NextConfig } from "next";
+
 const nextConfig = {
     reactStrictMode: false,
     output: 'standalone',
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'i.scdn.co',
-        },
-      ],  
-        domains: ['images.unsplash.com','i.ytimg.com'],
-      },
+    async headers() {
+      return [
+          {
+              // matching all API routes
+              source: "/api/:path*",
+              headers: [
+                  { key: "Access-Control-Allow-Credentials", value: "true" },
+                  { key: "Access-Control-Allow-Origin", value: "*" },
+                  { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS" },
+                  { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" }
+              ]
+          }
+      ]
+  },
+    // images: {
+    //   remotePatterns: [
+    //     {
+    //       protocol: 'https',
+    //       hostname: 'i.scdn.co',
+    //     },
+    //   ],  
+    //     domains: ['images.unsplash.com','i.ytimg.com'],
+    //   },
 };
 
 export default nextConfig;
